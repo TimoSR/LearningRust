3689d596e974b91ee7709ac9fbf17ded4bc9814e
TimoSR <timothy.s.rasmussen@gmail.com>
2025-04-29 00:15:17 +0200

    update

--- LearningModules/src/main.rs
+++ LearningModules/src/main.rs
@@ -1,15 +1,17 @@
 mod riftgate_network;
 mod utils;
 
+use riftgate_network::server;
+
 //use riftgate_network::{client::data_layer::send_data, server::ports::listen};
 //use utils::{helper_function, some_public_function};
 
 fn main() {
     println!("Starting main application");
-    
+
     utils::some_public_function();
     utils::helper_function();
-    riftgate_network::server::ports::listen();
+    server::ports::listen();
     riftgate_network::connect();
     riftgate_network::client::data_layer::send_data();
 
